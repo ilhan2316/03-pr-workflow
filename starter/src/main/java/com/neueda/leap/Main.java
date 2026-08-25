e2cd31440951f4cbf1a43b207b04178b2052cd81
ilhan2316 <ilhgelle@gmail.com>
2026-08-25 14:10:41 +0000

    Added unit tests

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,11 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "team";
-        System.out.println("Hello " + name + " from Sprint 1");
+        System.out.println(greetingForArgs(args));
+    }
+
+    static String greetingForArgs(String[] args) {
+        String name = (args != null && args.length > 0 && !args[0].isBlank()) ? args[0] : "team";
+        return "Hello " + name + " from Sprint 1";
     }
 }
